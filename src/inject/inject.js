c059d2e51fc97a26d1e5208bac6a0587e7f0d082
Erez Hochman <erez.hochman@gmail.com>
2017-09-15 13:44:56 +0300

    removed all of Facebooks "Suggested Posts" cuz it was too confusing

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,18 +4,17 @@ chrome.extension.sendMessage({}, function(response) {
       clearInterval(readyStateCheckInterval);
 	// ----------------------------------------------------------
 	// This part of the script triggers when page is done loading
-	console.log("Hello. This message was sent from scripts/inject.js");
-	console.log($);
 	var classes = [
 	  '.UFICommentActorName',
 	  '.fwb.fcg',
 	  '._5pb8._8o._8s.lfloat._ohe',
 	  '._ohe.lfloat',
-	  '.profileLink'
+	  '.profileLink',
+	  '._5pcr.fbUserPost:contains("Suggested Post")',
 	];
 	setInterval( () => {
 	  classes.forEach((e) => {
-	    $(e).hide();
+	    $(e).css('display','none');
 	  });
 	}, 350);
 	// ----------------------------------------------------------
